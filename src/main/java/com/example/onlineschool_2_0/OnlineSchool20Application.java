d2c664ef36795fdd1c034b2e1865a932664ba259
Massonus <127430447+Massonus@users.noreply.github.com>
2023-08-13 18:26:54 +0300

    Initial commit

---
+++ src/main/java/com/example/onlineschool_2_0/OnlineSchool20Application.java
@@ -0,0 +1,13 @@
+package com.example.onlineschool_2_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchool20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSchool20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineschool_2_0/OnlineSchool20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineschool_2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchool20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
